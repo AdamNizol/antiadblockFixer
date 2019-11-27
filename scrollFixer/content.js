801f14c9686eecbdb8dabbccc11d0a53987d280b
AdamNizol <32315521+AdamNizol@users.noreply.github.com>
2019-11-26 23:34:25 -0800

    add brute force

--- scrollFixer/content.js
+++ scrollFixer/content.js
@@ -1,37 +1,60 @@
 chrome.runtime.onMessage.addListener(function(request) {
+  let bodyElem = document.getElementsByTagName("body")[0]
+  let htmlElem = document.getElementsByTagName("html")[0]
+
   switch(request) {
     case "fixScrolling":
-      let bodyElem = document.getElementsByTagName("body")[0]
-      let htmlElem = document.getElementsByTagName("html")[0]
+
       console.log(bodyElem)
       console.log(htmlElem)
       let elems = [bodyElem, htmlElem]
 
       for(let i = 0; i<elems.length; i++){
-        elems[i].style.overflowY = "auto"
-        elems[i].style.overflow = "auto"
+        elems[i].style.setProperty("overflowY", "auto", "important")
+        elems[i].style.setProperty("overflow", "auto", "important")
       }
       break;
 
     case "bruteForce":
       // Get all elements that have a style attribute
-      var elms = document.querySelectorAll("*[style]");
+      var elms = document.querySelectorAll("*");
+      elms = Array.prototype.slice.call(elms)
+
+      let htmlElemPos = window.getComputedStyle(htmlElem, null).getPropertyValue("position") || "";
+      htmlElemPos = htmlElemPos.replace(/\s/g, "").toLowerCase();
+      console.log(htmlElemPos)
+      console.log(htmlElemPos == "fixed")
+      if(htmlElemPos == "fixed"){
+        htmlElem.style.setProperty("position", "absolute", "important")
+      }
+      elms.push(htmlElem);
 
        // Loop through them
        Array.prototype.forEach.call(elms, function(elm) {
          // Get the overflow value
-         var oflw = elm.style.overflow || "";
-         var oflwY = elm.style.overflowY || "";
+         var oflw = window.getComputedStyle(elm, null).getPropertyValue("overflow") || "";
+         var oflwY = window.getComputedStyle(elm, null).getPropertyValue("overflowY") || "";
 
          // Remove all whitespace, make it all lower case
          oflw = oflw.replace(/\s/g, "").toLowerCase();
          oflwY = oflwY.replace(/\s/g, "").toLowerCase();
 
+         //console.log(oflw)
+
          if( (oflw != "") || (oflw != "auto")){
-           elm.style.overflow = "auto"
+           elm.style.setProperty("overflow", "auto", "important")
          }
          if( (oflwY != "") || (oflwY != "auto")){
-           elm.style.overflowY = "auto"
+           elm.style.setProperty("overflowY", "auto", "important")
+         }
+
+         let pos = window.getComputedStyle(elm, null).getPropertyValue("position") || "";
+         let zIndex = window.getComputedStyle(elm, null).getPropertyValue("z-index") || "";
+
+         //console.log(zIndex)
+         //console.log(pos)
+         if(zIndex > 600 && pos == "fixed"){
+           elm.parentNode.removeChild(elm);
          }
 
       });
