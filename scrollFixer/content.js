1752c0c4b80b965b1d8b8eae8caea48646cda966
AdamNizol <32315521+AdamNizol@users.noreply.github.com>
2019-12-11 20:48:16 -0800

    set overflow back to auto

--- scrollFixer/content.js
+++ scrollFixer/content.js
@@ -27,10 +27,10 @@ chrome.runtime.onMessage.addListener(function(request) {
        oflwY = oflwY.replace(/\s/g, "").toLowerCase();
 
        if(oflw == "hidden"){
-         elm.style.setProperty("overflow", "initial", "important")
+         elm.style.setProperty("overflow", "auto", "important")
        }
        if( oflwY == "hidden" ){
-         elm.style.setProperty("overflow-y", "initial", "important")
+         elm.style.setProperty("overflow-y", "auto", "important")
        }
      }
 
